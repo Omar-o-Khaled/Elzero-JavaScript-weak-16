90c17907ba0ccc20f61754e376e8342dc04ed802
Omar Khaled <120221178+Omar-o-Khaled@users.noreply.github.com>
2023-02-02 16:03:42 +0200

    Add files via upload

---
+++ challenge-122/challenge.js
@@ -0,0 +1,22 @@
+let chosen = 3;
+
+let myFriends = [
+    { title: "Osama", age: 39, available: true, skills: ["HTML", "CSS"] },
+    { title: "Ahmed", age: 25, available: false, skills: ["Python", "Django"] },
+    { title: "Sayed", age: 33, available: true, skills: ["PHP", "Laravel"] },
+];
+
+let[a,b,c]=myFriends;
+
+
+if(chosen===1){
+    ({title:t,age:a,available:v,skills:[,lastskill]}=a)
+}else if(chosen===2){
+    ({title:t,age:a,available:v,skills:[,lastskill]}=b)
+}else{
+    ({title:t,age:a,available:v,skills:[,lastskill]}=c)
+}
+console.log(t);
+console.log(a);
+v ? console.log("Available") : console.log("NOT Available");
+console.log(lastskill);
